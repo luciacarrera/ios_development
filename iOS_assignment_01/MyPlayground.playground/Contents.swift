e51d16afce54ace0ec48254c66af221e925d3ee7
Lucia Carrera <100386238@alumnos.uc3m.es>
2021-09-14 12:07:11 -0400

    Created Chessboard

--- iOS_assignment_01/MyPlayground.playground/Contents.swift
+++ iOS_assignment_01/MyPlayground.playground/Contents.swift
@@ -37,6 +37,9 @@ print()
 /* FIRST PATTERN */
 let length: Int = 9
 let middle = Int(length/2)+1
+let outerDiamond = " "
+let innerDiamond = "-"
+let borderDiamond = "X"
 
 var str = ""
 var start = middle
@@ -44,11 +47,11 @@ var end = middle
 for i in 1...length{
     for j in 1...length{
         if j < start || j > end{
-            str += " "
+            str += outerDiamond
         }else if j > start && j < end{
-            str += "-"
+            str += innerDiamond
         }else{
-            str += "X"
+            str += borderDiamond
         }
     }
     print(str)
@@ -63,4 +66,38 @@ for i in 1...length{
     }
 }
 
+// space between exercises
+print()
+
+/* CHESSBOARD */
+let rows = 8
+let cols = 10
+let square1 = "X"
+let square2 = " "
+
+for current_row in 0...rows{
+    str = ""
+    for i in 1...cols{
+        str += "+-"
+        if i == cols{
+            str += "+"
+        }
+    }
+    str += "\n"
+
+    if current_row != rows{
+        var pattern = ""
+        if(current_row % 2 == 0){
+            pattern = "|" + square1 + "|" + square2
+        }else{
+            pattern = "|" + square2 + "|" + square1
+        }
+        for _ in stride(from: 1, to: cols, by: 2){
+            str += pattern
+        }
+        str += "|"
+        }
+    print(str)
+    str = ""
+}
 // Cheatsheet for keyboard: ><{}|@#~€
