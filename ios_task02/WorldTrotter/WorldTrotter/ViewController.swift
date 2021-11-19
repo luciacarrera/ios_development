49273f5ecd9e4e747b0d29419a3a24f2a3482115
Lucía Carrera <61793180+luciacarrera@users.noreply.github.com>
2021-11-18 19:12:21 -0500

    a start

---
+++ ios_task02/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Lucia Carrera Saenz on 11/18/21.
+//  Copyright © 2021 Lucia Carrera Saenz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+}
+
